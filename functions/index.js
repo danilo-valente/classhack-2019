e6959d492e0b7f174befeed01d1b74d8403f3fc3
Danilo Valente <danilovalente96@gmail.com>
2019-08-24 12:25:14 -0300

    Configure express

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
+const firebase = require('firebase-admin');
 const functions = require('firebase-functions');
+const express = require('express');
+const engines = require('consolidate');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const firebaseApp = firebase.initializeApp(
+    functions.config().firebase
+);
+ 
+const app = express();
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.get('/', function (request, response) {
+    response.render('index', {});
+});
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.app = functions.https.onRequest(app);
